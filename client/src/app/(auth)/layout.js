6c0d299e405158f09b53a210535ffe57597d4972
BoweJordyn-FS <jkbowe@student.fullsail.edu>
2026-09-07 17:34:30 -0400

    feat: implement AuthLayout component and add Login and Playlists pages

--- client/src/app/(auth)/layout.js
+++ client/src/app/(auth)/layout.js
@@ -1,10 +1,8 @@
 export default function AuthLayout({ children }) {
 	return (
-		<html lang="en">
-			<body className="grid grid-cols-2">
-				<div></div>
-				<div>{children}</div>
-			</body>
-		</html>
+		<div className="grid grid-cols-2">
+			<div></div>
+			<div>{children}</div>
+		</div>
 	);
 }
---
+++ client/src/app/(auth)/login/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Login() {
+	return (
+		<main className="flex flex-col justify-center h-full p-10">
+			<h1 className="text-3xl font-bold">Log In</h1>
+		</main>
+	);
+}
+
+export default Login;
